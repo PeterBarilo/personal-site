3c44baa6cb746dcf07e076bac1898c8c8e785b74
Peter Barilo <peterbarilo@gmail.com>
2024-04-17 21:04:08 -0500

    added favicon

--- src/landing.js
+++ src/landing.js
@@ -26,7 +26,7 @@ function Landing() {
             }}
         />
         </div>
-        <div className={`fixed top-40 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-.75 text-terminal text-l ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
+        <div className={`fixed top-40 left-1/2 mr-20 mt-16 transform -translate-y-1/2 -translate-x-1/2 text-terminal text-l ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
             <h2 className='mt-48'>Hello!</h2>
             <br></br>
             <p>My name is Peter Barilo and I am a undergraduate studying computer science at the University of Wisconsin-Madison.</p>
@@ -37,7 +37,7 @@ function Landing() {
         
         </div>
 
-        <div className={`flex justify-center space-x-20 mt-96 pt-14 ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
+        <div className={`flex justify-center space-x-20 pt-28 mt-96 pt-14 ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
         <a href="https://github.com/PeterBarilo" target="_blank" rel="noopener noreferrer" className="mr-4 relative">
             <img src={githubLogoHover} alt="GitHub" className="w-14 h-14" />
             <img src={githubLogoDefault} alt="GitHub" className="w-14 h-14 absolute top-0 left-0 opacity-0 transition-opacity duration-300 hover:opacity-100" />
