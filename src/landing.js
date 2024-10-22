eb591827f729e122c9f4a20d04935a54c7e0efb8
Peter Barilo <peterbarilo@gmail.com>
2024-10-22 17:03:56 -0500

    small fix

--- src/landing.js
+++ src/landing.js
@@ -26,7 +26,7 @@ function Landing() {
             }}
         />
         </div>
-        <div className={`flex flex-col flex-auto px-8 2xl:px-[25vw] xl:px-80 lg:px-40 md:px-20 sm:px-16  mt-20 xl:mt-40 lg:mt-28 md:mt-24 sm:mt-20 text-terminal text-l ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
+        <div className={`flex flex-col flex-auto px-8 2xl:px-[25vw] xl:px-80 lg:px-40 md:px-20 sm:px-16  mt-20 xl:mt-28 lg:mt-24 md:mt-24 sm:mt-20 text-terminal text-l ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
             <h2 className=''>Hello!</h2>
             <br></br>
             <p>My name is Peter Barilo and I am an undergraduate studying computer science at the University of Wisconsin-Madison.</p>
@@ -39,7 +39,7 @@ function Landing() {
         
         </div>
 
-        <div className={`flex flex-auto mt-10 lg:mb-[-100px] sm:mb-[-0px] justify-center ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
+        <div className={`flex flex-auto mt-10 lg:mb-[-50px] sm:mb-[-0px] justify-center ${show ? 'opacity-100' : 'opacity-0'}`} style={{ transition: 'opacity 2s' }}>
           <a href="https://github.com/PeterBarilo" target="_blank" rel="noopener noreferrer" className="mr-4 relative">
               <img src={githubLogoHover} alt="GitHub" className="w-14 h-14" />
               <img src={githubLogoDefault} alt="GitHub" className="w-14 h-14 absolute top-0 left-0 opacity-0 transition-opacity duration-300 hover:opacity-100" />
