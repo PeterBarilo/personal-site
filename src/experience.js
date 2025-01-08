bb3db559c020126d17ccf1532b7c8d5ebb2e7fd9
Peter Barilo <peterbarilo@gmail.com>
2025-01-07 18:29:45 -0800

    updated expereince

--- src/experience.js
+++ src/experience.js
@@ -10,18 +10,21 @@ function Experience() {
       {
         name: 'Software Engineer',
         company: 'AbbVie',
-        description: "•	Developed and deployed scalable solutions using Python, AWS, and Docker with contributions to key projects. I integrated Large Language Models into analytical workflows by fine-tuning pre-trained models and optimizing infrastructure. I implemented CI/CD pipelines using AWS CodePipeline and Docker",
+        date:  'Jan 2025 - Present',
+        description: "Developed and deployed scalable solutions using Python, AWS, and Docker with contributions to key projects. I integrated Large Language Models into analytical workflows by fine-tuning pre-trained models and optimizing infrastructure. I implemented CI/CD pipelines using AWS CodePipeline and Docker",
         link: "https://www.linkedin.com/company/abbvie/"
       },
         {
           name: 'Machine Learning and Java Instructor',
           company: 'iD Tech',
+          date:  'May 2023 - Sep 2023',
           description: 'I led comprehensive instruction in Python-based Machine Learning and advanced Java for a clientele who collectively spent over $75,000. I further drove the enhancement of my managerial skills and leadership prowess, alongside the continuous expansion of technical proficiencies.',
           link: "https://www.linkedin.com/company/id-tech-camps/"
         },
         {
           name: 'Co-founder, Software Engineer',
           company: 'Developer Secure',
+          date:  'Sep 2023 - Aug 2024',
           description: "I developed a business model that helps local comapnies with improving their online presence. We focuse on Web Development, ad creation, and SEO management.",
           link: "https://www.linkedin.com/company/developer-secure/"
         },
